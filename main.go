1cc20e6067f9d53cee2b73a90c6c0b5466e0bf60
awhitwam <whitwam@gmail.com>
2020-05-14 13:53:25 +0100

    test

--- main.go
+++ main.go
@@ -3,23 +3,17 @@ package main
 import (
 	"encoding/csv"
 	"github.com/bogdanovich/dns_resolver"
-	"log"
 	"math/rand"
 	"os"
+	"text/tabwriter"
 	"time"
 )
 
 //aw test
 
 import (
-	"encoding/csv"
 	"fmt"
-	"github.com/bogdanovich/dns_resolver"
-	"log"
-	"math/rand"
-	"os"
 	"strings"
-	"time"
 )
 
 func main() {
@@ -29,23 +23,38 @@ func main() {
 	resolver := dns_resolver.New(server)
 	lines := LoadSites()
 	file, err := os.Create(filename)
+	cntrtotal := 0
+	cntrbad := 0
 	_ = err
+	lastbad := ""
+	tabout := new(tabwriter.Writer)
+	tabout.Init(os.Stdout, 8, 8, 0, '\t', 0)
 
+	fmt.Printf("%s", server)
 	for {
 		site := lines[rand.Intn(2000)][1]
 		resolver.RetryTimes = 1
 		start := time.Now()
 		ip, err := resolver.LookupHost(site)
 		duration := time.Since(start)
-
+		cntrtotal = cntrtotal + 1
+		if err != nil {
+			cntrbad = cntrbad + 1
+			lastbad = start.Format("15:04:05")
+		}
 		time.Sleep(200)
-		_ = ip
 
-		log.Printf("%s >%s< in %d--%s", server, site, duration.Milliseconds(), err)
+		//log.Printf("%s >%s< in %d--%s", server, site, duration.Milliseconds(), err)
 		var myoutput string = fmt.Sprintf("%s,%s,%s,%d,%s", start.Format(("2006-01-02 15:04:05")), server, site, duration.Milliseconds(), err)
+		//fmt.Printf("\r",myoutput)
+		//fmt.Printf("\rTotal:%d Bad:%d   Working:%s  Last Fail:%s                              ",cntrtotal,cntrbad,site,lastbad)
+		defer tabout.Flush()
+		fmt.Fprintf(tabout, "\rTotal:%d\tBad:%d\tLfail:%s\tLookup:%s\t                 ", cntrtotal, cntrbad, lastbad, site)
+		tabout.Flush()
 		i, err := file.WriteString(myoutput + "\r\n")
 		_ = i
 		_ = err
+		_ = ip
 
 	}
 	file.Close()
